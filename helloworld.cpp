20c3c2cbce260546f52e7f3aba3af85bbd861c62
AmeyDesai01 <ameyeknathdesai@gmail.com>
2022-07-30 19:29:39 +0530

    second commit

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,1 +1,26 @@
-fhifdkfakld
+#include <iostream>
+using namespace std;
+
+int getFactorial(int n)
+{
+    //base condition
+    if(n<=1)
+
+    return 1;
+
+    //recursive call
+    int answer= n * getFactorial(n-1);
+    return answer;
+
+    
+}
+
+int main()
+{
+    int n;
+    cout<<"Please enter the Input"<<endl;
+    cin>>n;
+
+    cout<<"value of "<<n<<"! is " << getFactorial(n) <<endl;
+}
+
